61eabee44fe23258421bec03b34c60e41a3af6eb
Nayan Jagetiya <nayan.jagetiya@brevitaz.com>
2025-06-01 14:31:59 +0530

    Implement security using okta and remove Auth0

--- src/main/java/com/nayan/obai/gateway/config/SecurityConfig.java
+++ src/main/java/com/nayan/obai/gateway/config/SecurityConfig.java
@@ -18,12 +18,13 @@ public class SecurityConfig
 		return http
 				.authorizeExchange(auth -> auth
 						// login and oauth2 endpoints will be used by spring so need to permit them
-						.pathMatchers("/login**", "/oauth2/**").permitAll()
+						.pathMatchers("/login**", "/oauth2/**","/swagger-ui/**").permitAll()
 						.anyExchange().authenticated()
 				)
-				.oauth2Login(Customizer.withDefaults())
+				.oauth2Login(Customizer.withDefaults())             // handles browser login via Okta
 				// this will required as we want to authenticate using client/browser
-				.oauth2Client(Customizer.withDefaults())
+				.oauth2Client(Customizer.withDefaults())            // enables TokenRelay to forward access tokens
+//				.oauth2ResourceServer(Customizer.withDefaults())    // validates tokens (e.g., for API access)
 				.logout(logout -> logout
 						.logoutSuccessHandler(new RedirectServerLogoutSuccessHandler())
 				)
